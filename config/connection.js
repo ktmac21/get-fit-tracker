6e700c382940bd1b06842ab39760c3cf44f54eb8
Katie Eagles <kteagles21@gmail.com>
2021-10-08 15:18:38 -0400

    updated config

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 const { connect, connection } = require('mongoose');
 
-connect(process.env.MONGO_UI || 'mongodb://localhost/workout', {
+connect(
+  process.env.MONGO_UI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
