c5773ba642b137fb3f5e630797d775a793f8f1bd
Katie Eagles <kteagles21@gmail.com>
2021-12-15 15:14:37 -0500

    deployment

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
